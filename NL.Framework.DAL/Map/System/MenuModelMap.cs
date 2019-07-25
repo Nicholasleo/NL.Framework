365170434b304c4b0034767caa2f1857202c6087
NicholasLeo <nicholasleo1030@163.com>
2019-07-25 23:55:22 +0800

    111

---
+++ NL.Framework.DAL/Map/System/MenuModelMap.cs
@@ -0,0 +1,53 @@
+﻿//***********************************************************
+//    作者：Nicholas Leo
+//    E-Mail:nicholasleo1030@163.com
+//    GitHub:https://github.com/nicholasleo
+//    时间：2019-07-25 18:34:11
+//    说明：
+//    版权所有：个人
+//***********************************************************
+using NL.Framework.Model.System;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.ModelConfiguration;
+
+namespace NL.Framework.DAL.Map
+{
+    public class MenuModelMap : EntityTypeConfiguration<MenuModel>
+    {
+        public MenuModelMap()
+        {
+            ToTable(TableName._MENU);
+
+            HasKey(t => t.Fid);
+
+            Property(t => t.Fid).HasColumnName("MenuId").HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+
+            Property(t => t.MenuName).HasColumnType("NVARCHAR").HasMaxLength(50);
+
+            Property(t => t.MenuUrl).HasColumnType("NVARCHAR").HasMaxLength(200);
+
+            Property(t => t.MenuIcon).HasColumnType("NVARCHAR").HasMaxLength(100);
+
+            Property(t => t.MenuParentId);
+
+            Property(t => t.MenuIcon).HasColumnType("NVARCHAR").HasMaxLength(50);
+
+            Property(t => t.MenuIsShow);
+
+            Property(t => t.MenuIndex);
+
+            Property(t => t.CreateTime).HasColumnType("DATETIME2");
+
+            Property(t => t.CreatePerson).HasColumnType("NVARCHAR").HasMaxLength(30);
+
+            Property(t => t.ModifyPerson).HasColumnType("NVARCHAR").HasMaxLength(30);
+
+            Property(t => t.ModifyTime).HasColumnType("DATETIME2");
+
+            HasMany(t => t.MenuFunctionModels).WithRequired(t => t.MenuModel).HasForeignKey(t => t.MenuId);
+
+            HasMany(t => t.RoleMenuFunctionModels).WithRequired(t => t.MenuModel).HasForeignKey(t => t.MenuId);
+
+        }
+    }
+}
